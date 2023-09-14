64359f73b64843e99b395e69c1063c2adefed9f5
alimuhamm <alimuhammadabuamer@gmail.com>
2023-09-14 06:08:04 +0300

    Create Main (1).c

---
+++ Main (1).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char ch ;
+    scanf ("%c",&ch);
+    char s [100];
+    scanf ("%s\n",&s);
+    char sen[100];
+scanf("%[^\n]", sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);
+}
